8dbe27752dbf7cf66279327514f87e12ce9f0d81
Muhammad Fadhil Maulana <95394393+ucupofcoffee@users.noreply.github.com>
2022-08-28 08:53:07 +0700

    Delete Section 3 directory

--- Section 3/Lesson 4,5,6/node_modules/input.js
+++
@@ -1,6 +0,0 @@
-const command = process.argv[2]
-if (command === 'add') {
- console.log('Adding note!')
-} else if (command === 'remove') {
- console.log('Removing note!')
-}
--- Section 3/Lesson 4,5,6/npm.js
+++
@@ -1,2 +0,0 @@
-const validator = require('validator')
-console.log(validator.isURL('https://mead.io')) // Print: true^%
